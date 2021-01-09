5942b2a9c8971f1c560e8cc756d3ee1b53e936e5
Matt Chang <matthewhansol@gmail.com>
2021-01-08 22:04:59 -0500

    fix: update to port

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const {db} = require('./server/db')
 const app = require('./server')
 const PORT = process.env.PORT || 5409
 
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+db.sync()
   .then(() => {
     console.log('db synced')
-    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+    app.listen(process.env.PORT || 5409, () => console.log(`studiously serving silly sounds on port ${PORT}`))
 })
