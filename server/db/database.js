4b54ae2a7101a70f094f3e1f46471e62a1a7057a
Matt Chang <matthewhansol@gmail.com>
2021-01-08 21:46:39 -0500

    fix: changed port to process.env.NODE_ENV

--- server/db/database.js
+++ server/db/database.js
@@ -11,8 +11,7 @@ const pkg = require('../../package.json');
 // playing aound with the app in the browser. We'll check to see if the node
 // node environment is 'test', in which case we'll use the test database.
 // Otherwise, the app connects with the normal database.
-// const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
-const dbName = 'postgresql-rectangular-07134'
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
