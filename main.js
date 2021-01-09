251be83b54b638e67592841fa8e14958665cb95b
Matt Chang <matthewhansol@gmail.com>
2021-01-08 21:59:36 -0500

    fix: changed port to process.env.PORT

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const {db} = require('./server/db')
 const app = require('./server')
-const PORT = 5409
+const PORT = process.env.PORT || 5409
 
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
